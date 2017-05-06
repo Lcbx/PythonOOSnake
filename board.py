3db9cc59357720776cb494333862d236683c8078
Luc Courbariaux <luc.courbariaux@polymtl.ca>
2017-05-06 19:07:37 -0400

    cleaner logic

--- board.py
+++ board.py
@@ -6,9 +6,9 @@ import food
 class Board:
 	
 	#state constants
-	DEATH = -1
-	NORMAL = 0
-	SNACK = 1
+	DEATH = -100
+	NORMAL = -1
+	SNACK = 10
 	
 	#background
 	color = (50, 30, 10)
@@ -28,16 +28,34 @@ class Board:
 	
 	#state manipulation
 	def update(self):
+		# does the snake collide with itself?
 		snakeCollidesSnake = not self.snake.move()
-		x, y = self.snake.body[0]
+		x, y = self.snake.getHead()
+		# does the snake collide with a wall?
 		snakeCollidesWall = x < 0 or x >= self.width or y < 0 or y >= self.height
+		# if so it's dead KAPUT
 		if snakeCollidesSnake or snakeCollidesWall:
 			return self.DEATH
-		if self.snake.body[0] == self.food.position:
+		#i t's a different story with food
+		if (x, y) == self.food.position:
+			# the snake grows
 			self.snake.grow()
+			# new food somewhere else
 			self.food = food.Food(self.width, self.height)
 			return self.SNACK
+		# nah just usual snaky stuff
 		return self.NORMAL
 	
 	def clone(self):
 		return copy.deepcopy(self)
+	
+	#important for the AI
+	def nextState(self, input):
+		clone = self.clone()
+		clone.command(input)
+		clone.update()
+		return clone
+	
+	
+	
+	
--- snake.py
+++ snake.py
@@ -5,6 +5,7 @@ class Snake:
 
 	headColor = (10, 255, 10)
 	bodyColor = (0, 100, 30)
+	deadColor = (200, 10, 255)
 		
 	def __init__(self, x = 10, y = 10, length = 3):
 		self.direction = direction.right()
@@ -12,19 +13,28 @@ class Snake:
 		self.growLength = 0
 		for i in range(0, length):
 			self.body.append((x - i, y))
-
-	def move(self):
-		"""Move the snake in the given direction."""
-		position = self.body[0] # get old position of head
-		x, y = self.direction.addTo(position) # calculate new position of head
-		if self.growLength > 0:
-			self.growLength -= 1 # grow
-		else:
-			self.body.pop() # remove tail
-		self.body.insert(0, (x, y)) # insert new position of head
-		if self.body[0] in self.body[1:]: # does the head collide with the rest of the body?
-			return False # yes, then this is an invalid move
-		return True # no
-
+	
+	def getHead(self):
+		return self.body[0]
+	
 	def grow(self, length = 1):
 		self.growLength = length
+	
+	def move(self):
+		# get old position of head
+		position = self.getHead()
+		# calculate new position of head
+		x, y = self.direction.addTo(position) 
+		# insert new position of head
+		self.body.insert(0, (x, y)) 
+		# if the snake isn't growing, get rid of last tail
+		if self.growLength == 0:
+			self.body.pop()
+		# otherwise decrease grow count
+		else: 
+			self.growLength -= 1
+		
+		# does the new head collide with the rest of the body?
+		if (x, y) in self.body[1:]: 
+			return False # yes, then this is an invalid move
+		return True # no
